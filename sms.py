f574fbd7ac3ed80a356b221d04454e1e23973832
Prajwal Saokar <prajwalsaokar@gmail.com>
2022-07-13 09:54:43 -0400

    completed sms notification gateway

--- sms.py
+++ sms.py
@@ -2,7 +2,11 @@ import os
 from twilio.rest import Client
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from dotenv import load_dotenv
 import rpi
+
+load_dotenv()
+
 
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -14,9 +18,9 @@ def notify(image_path):
     message = client.messages \
         .create(
         body="Threat Detected. Should lockdown be initiated?",
-        media_url = image_path,
+        media_url = [image_path],
         from_  = '+12056289601',
-        to = '+16783926866'
+        to = '+14705899932'
     )
 @app.route("/sms", methods=["GET", "POST"])
 def receive_sms():
